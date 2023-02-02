66a6189fedd75555794ad0c5a1901e3917d17827
letsjo <hyunoh.jo@gmail.com>
2023-02-02 13:58:15 +0900

    feat(*): Day 2/100

---
+++ [100일챌린지] 003 나이 출력/script.js
@@ -0,0 +1,31 @@
+/**
+ * ! [JavaScript 알고리즘 100일 챌린지]
+ * https://www.youtube.com/playlist?list=PLkfUwwo13dlWZxOdbvMhkzhAowaiEjuGS
+ * 
+ * ! 문제
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120820
+ */
+
+// 내가 푼 정답
+
+function solution(age) {
+    const nowYear = 2022;
+    return nowYear - age + 1;
+}
+
+console.log(solution(40));
+
+// 추천 풀이
+
+function recommend(age) {
+    return 2023 - age;
+}
+
+console.log(recommend(40));
+
+
+/**
+ * ! 설명
+ * 
+ * 
+ */
