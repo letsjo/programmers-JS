250e03dea67090964bb2491c9c3c4c6863a1efc8
letsjo <hyunoh.jo@gmail.com>
2023-02-02 13:53:23 +0900

    feat(*): Day 1/100

---
+++ [100일챌린지] 숫자 비교하기/script.js
@@ -0,0 +1,31 @@
+/**
+ * ! [JavaScript 알고리즘 100일 챌린지]
+ * https://www.youtube.com/playlist?list=PLkfUwwo13dlWZxOdbvMhkzhAowaiEjuGS
+ * 
+ * ! 문제
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120807
+ */
+
+// 내가 푼 정답
+
+function solution(num1, num2) {
+    return num1 === num2 ? 1 : -1;
+}
+
+console.log(solution(7, 2));
+
+// 추천 풀이
+
+function recommend(num1, num2) {
+    return num1 === num2 ? 1 : -1;
+}
+
+console.log(recommend(7, 2));
+
+
+/**
+ * ! 설명
+ * 
+ * 삼항연산자 사용
+ * 
+ */
